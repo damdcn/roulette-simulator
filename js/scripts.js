ae3b8c905bbc879bc976e50561cd8da16de4515c
Damien <damiend34@hotmail.fr>
2020-06-23 19:25:52 +0200

    ajout plateau de jeu

--- js/scripts.js
+++ js/scripts.js
@@ -10,6 +10,7 @@ var actualLooseSuit = 0;       // suite perdante actuelle
 var maxLooseSuit = 0;          // suite perdante la plus longue
 var resSum = "";
 var resDetail = "";
+var actualBet = "";
 
 
 function clearValues(){
@@ -23,6 +24,9 @@ function clearValues(){
     maxWallet = 0;
     actualLooseSuit = 0;
     maxLooseSuit = 0;
+    actualBet = "";
+    resSum = "";
+    resDetail = "";
 }
 
 function setWalletStart(n){
@@ -66,8 +70,6 @@ $('#submit').on('click', function(){
     let play = true;
     let nbTirage = $('#nb-tirage').val();
     setWalletStart($('#balance-depart').val());
-    resSum = "";
-    resDetail = "";
     let successRound = new Array();
     let valJeton = $('#val-jeton').val();
     while(document.getElementById('res-sum').firstChild){
@@ -76,23 +78,29 @@ $('#submit').on('click', function(){
     while(document.getElementById('res-detail').firstChild){
         document.getElementById('res-detail').removeChild(document.getElementById('res-detail').firstChild);
     }
-    for(i=0;i<=36;i++){
-        let c = document.getElementById('c'+i).getElementsByTagName("input")[0].value;
-        if(c && c != 0){
-            let betOn = Math.round(c*valJeton*100) / 100;
-            resSum += "<label>Mise sur le "+i+" : "+c+" jeton(s) soit "+betOn+"€</label><br>";
-            misePlateau[i] = betOn;
-            mise += betOn;
-            nbJeton += (c*1);
-        } else {
-            misePlateau[i] = 0.0;
-        }
+    while(document.getElementById('actual-bet').firstChild){
+        document.getElementById('actual-bet').removeChild(document.getElementById('actual-bet').firstChild);
     }
-    resSum += "<label><br>Mise totale : "+nbJeton+" jeton(s) soit "+mise+"€</label><br>";
+
+    for(let i=0;i<=36;i++){
+        misePlateau[i] = 0.0;
+    }
+    let chips = document.getElementsByClassName('chip');
+    for(chip of chips){
+        let i = parseInt(chip.id.substr(4));
+        let c = parseInt(chip.firstChild.nodeValue);
+        let betOn = Math.round(c*valJeton*100) / 100;
+        actualBet += "<label>Mise sur le "+i+" : "+c+" jeton(s) soit "+betOn+"€</label><br>";
+        misePlateau[i] = betOn;
+        mise += betOn;
+        nbJeton += (c*1);
+    };
+
+    actualBet += "<label><br>Mise totale : "+nbJeton+" jeton(s) soit "+mise+"€</label><br>";
 
     if(mise>0 && nbTirage>0 && walletStart>0){
-        for(i=0;i<nbTirage;i++) {
-            resDetail += "<label>----------------------------------------------------</label><br>";
+        for(let i=0;i<nbTirage;i++) {
+            resDetail += "<label>-------------------------------------------</label><br>";
             resDetail += "<div id=p"+(i+1)+" class=p"+(i+1)+">";			
             resDetail += "<label>"+(i+1)+"ème partie</label><br><br>";			
             resDetail += "<label>Wallet debut partie = "+(wallet+gain)+"</label><br>";
@@ -126,7 +134,7 @@ $('#submit').on('click', function(){
         }
         
         let total = wallet + gain;
-        resSum += "<br><br><label>============= Recap =============</label><br><br>";
+        resSum += "<label>============= Recap =============</label><br><br>";
         resSum += "<label>Wallet debut partie = "+walletStart+"€</label><br>";
         resSum += "<label>Partie(s) Jouée(s) : "+nbPartie+" [ /"+nbTirage+" partie(s) lancée(s) ]</label><br>";
         resSum += "<label>Partie(s) Gagnée(s) : "+win+"</label><br>";
@@ -137,13 +145,44 @@ $('#submit').on('click', function(){
         resSum += "<br><br><label>Vous devez : <ul><li>Mise sur au moins un nombre</li><li>Définir le nombre de tirage</li><li>Définir la balance de départ</li></ul></label><br>";
     }
 
+    $('#actual-bet').append(actualBet);
     $('#res-sum').append(resSum);
     $('#res-detail').append(resDetail);
     
     setTimeout(function(){
         for(j=0;j<successRound.length;j++){
             document.getElementById('p'+successRound[j]).style.backgroundColor = "#9cffaa";
+            document.getElementById('p'+successRound[j]).style.color = "black";
         }
     }, 100);
 });
 
+$('#reset-button').on('click', function(){
+    clearValues();
+    while(document.getElementById('res-sum').firstChild){
+        document.getElementById('res-sum').removeChild(document.getElementById('res-sum').firstChild);
+    }
+    while(document.getElementById('res-detail').firstChild){
+        document.getElementById('res-detail').removeChild(document.getElementById('res-detail').firstChild);
+    }
+    while(document.getElementById('actual-bet').firstChild){
+        document.getElementById('actual-bet').removeChild(document.getElementById('actual-bet').firstChild);
+    }
+    $('.chip').remove();
+});
+
+function placeChip(e){ // place un jeton sur la case e
+    let c = e.id.substr(1); // numéro de la case
+    var chipElement = '<div class="chip" id="chip'+c+'">1</div>';
+    if(e.firstChild.tagName != 'DIV'){
+        $('#'+e.id).prepend(chipElement);
+        //$('#res-sum').append("<label>Mise sur le "+c+" : 1 jeton soit "+betOn+"€</label><br>");
+    } else {
+        let n = parseInt(document.getElementById(e.firstChild.id).firstChild.nodeValue); // nombre de jetons sur c
+        n++;
+        $('#'+e.firstChild.id).empty().append(n);
+        if(n>=5 && n<10) $('#'+e.firstChild.id).css("background", "#0000f2")
+        if(n>=10 && n<50) $('#'+e.firstChild.id).css("background", "#7800a1")
+        if(n>=50 && n<100) $('#'+e.firstChild.id).css("background", "#008f0a")
+    }
+}
